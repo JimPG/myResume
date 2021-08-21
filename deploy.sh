b519a98332dc705c76273a60354f23569aa593cd
JimPeng <jimpeng1996@gmail.com>
2021-08-21 15:29:14 +0800

    second

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env sh
+
+# 當發生錯誤時終止腳本運行
 set -e
+
+# 打包
 npm run build
+
+# 移動至到打包後的dist目錄 
 cd dist
 git init
-git add -addgit commit -m 'deploy'
-git push -f git@github.com:JimPG/myResume.git
+git add -A
+git commit -m 'deploy'
+
+# 部署到 https://github.com/chou0728/eric-project.git 分支為 gh-pages
+git push -f https://git@github.com:JimPG/myResume.git master:gh-pages
+
+cd -
